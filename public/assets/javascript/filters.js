9fa2894056895825f4c6079146968d6cd6d243f3
Styvens Wentz <styvens.wentz@gmail.com>
2024-07-23 10:25:45 +0200

    correction done

--- public/assets/javascript/filters.js
+++ public/assets/javascript/filters.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('DOM fully loaded and parsed');
-
     document.getElementById('categoryFilter').addEventListener('change', function () {
         const selectedCategory = this.value;
         const mediaType = document.getElementById('media_type').value;
