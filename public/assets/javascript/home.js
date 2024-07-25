29d69c2ddac01302d4bfc726e2546cd12f635b10
RomainGuirous <romaing_59@hotmail.fr>
2024-07-25 09:20:36 +0200

    index

--- public/assets/javascript/home.js
+++ public/assets/javascript/home.js
@@ -1,7 +1,21 @@
-const slidesContainer = document.getElementById("slides-container");
+// gestion images
+const img = document.querySelectorAll('.slide_picture');
+for (let i = 0; i < img.length; i++) {
+  if (img[i].width < img[i].height) {
+    img[i].style.height = "70vh";
+    img[i].style.width = "auto";
+    // img[i].style.max-width = "70vw";
+    // img[i].style.max-height = "70vw";
+  }
+}
+console.log(img);
+
+
+// carrousel
+const slidesContainer = document.getElementById("slides_container");
 const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("fleche-gauche");
-const nextButton = document.getElementById("fleche-droite");
+const prevButton = document.getElementById("fleche_gauche");
+const nextButton = document.getElementById("fleche_droite");
 nextButton.addEventListener("click", () => {
   const slideWidth = slide.clientWidth;
   slidesContainer.scrollLeft += slideWidth;
